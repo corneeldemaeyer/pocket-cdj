cc27755d4f2ec981dcf8ddb250caad89abbf708f
corneeldemaeyer <79110278+corneeldemaeyer@users.noreply.github.com>
2026-07-22 00:18:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pocket-cdj-v6';
+const CACHE = 'pocket-cdj-v7';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
 
 self.addEventListener('install', e => {
